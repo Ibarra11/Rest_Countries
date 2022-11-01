1a5f1cd631b28cd47a54f11c01683f82f11b8160
Alan Ibarra <alan.ibarra209@gmail.com>
2022-11-01 15:13:46 -0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+
 
 module.exports = nextConfig
