4581d5e3774ce1f0fd172493f1ea09268ecf044a
Alan Ibarra <alan.ibarra209@gmail.com>
2022-11-03 15:58:26 -0700

    toggle dark mode was not working with server components

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,22 +1,57 @@
 "use client";
+import { Moon } from "lucide-react";
 import { useEffect, useState } from "react";
 export default function Header() {
   const [darkMode, setDarkMode] = useState(false);
+
   useEffect(() => {
-    if (!darkMode) {
-      document.documentElement.classList.add("dark");
+    if (localStorage.getItem("darkMode")) {
+      const mode = localStorage.getItem("darkMode");
+
+      if (mode === "true") {
+        setDarkMode(true);
+        document.documentElement.classList.add("dark");
+      } else {
+        setDarkMode(false);
+        document.documentElement.classList.remove("dark");
+      }
+    } else if (window.matchMedia) {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        localStorage.setItem("darkMode", "true");
+        setDarkMode(true);
+        document.documentElement.classList.add("dark");
+      } else {
+        localStorage.setItem("darkMode", "false");
+        setDarkMode(false);
+        document.documentElement.classList.remove("dark");
+      }
     } else {
+      localStorage.setItem("darkMode", "false");
+      setDarkMode(false);
       document.documentElement.classList.remove("dark");
     }
-  }, [darkMode]);
+  }, []);
+
+  function toggleDarkMode() {
+    if (!darkMode) {
+      localStorage.setItem("darkMode", "true");
+      setDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("darkMode", "false");
+      setDarkMode(false);
+      document.documentElement.classList.remove("dark");
+    }
+  }
+
   return (
     <header className="flex items-center h-20 dark:bg-gray-2 dark:text-white xl:mb-12 xl:px-20">
       <h1 className=" text-2xl font-extrabold">Where in the world?</h1>
-      <div className="flex gap-2 ml-auto ">
-        <button onClick={() => setDarkMode(!darkMode)}>toggle</button>
-        <p className="text-base font-semibold">
-          {darkMode ? "Light Mode" : "Dark Mode"}
-        </p>
+      <div className="flex items-center gap-2 ml-auto ">
+        <button onClick={() => toggleDarkMode()}>
+          <Moon size={16} />
+        </button>
+        <p className="text-base font-semibold">Dark Mode</p>
       </div>
     </header>
   );
